aba479e19211d9ecd5bb941c54f198fe7e19874b
ProudTurtle <a.wierzbicka0579@gmail.com>
2022-03-16 15:04:55 +0100

    Wersja 1.1

--- PSM01/src/com/company/Main.java
+++ PSM01/src/com/company/Main.java
@@ -3,35 +3,33 @@ package com.company;
 import java.util.Objects;
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Wybierz co chcesz policzyć: \n 1. Sin(x) \n 2. Cos(x)");
-        Scanner input = new Scanner(System.in);
+        out.println("Wybierz co chcesz policzyć: \n 1. Sin(x) \n 2. Cos(x)");
+        Scanner input = new Scanner(in);
         String text = input.nextLine();
 
+        out.println("Podaj x: ");
+        String text1 = input.nextLine();
+        out.println("Podaj n: ");
+        String text2 = input.nextLine();
+
+        double x= Double.parseDouble(text1);
+        int n = Integer.parseInt(text2);
 
         if(Objects.equals(text, "1"))
             {
-            System.out.println("Podaj x i n");
-                Scanner input1 = new Scanner(System.in);
-                String text1 = input.nextLine();
-                Scanner input2 = new Scanner(System.in);
-                String text2 = input.nextLine();
-
-                double x= Double.parseDouble(text1);
-                int n = Integer.parseInt(text2);
-
-                System.out.println("Wynik sin(x) to: " + Liczenie.Sin(x, n));
+               out.println("Wynik sin(x) to: " + Liczenie.Sin(x, n));
 
 
 
             }else
             {
-            System.out.println("Podaj n i k\"");
-                Scanner input1 = new Scanner(System.in);
-                String text1 = input.nextLine();
+                out.println("Wynik cos(x) to: " + Liczenie.Cos(x, n));
             }
 
 
@@ -57,6 +55,20 @@ class Liczenie {
         }
         return sum;
     }
+
+    public static double Cos(double x, int n) {
+        double sum = 0.0;
+
+        for (int k = 0; k <= n; k++) {
+            double poww = Math.pow(x, (2 * k));
+            double licznik = Math.pow(-1, k);
+            licznik = licznik * poww;
+            double mianownik = silnia((2 * k));
+            sum += (licznik / mianownik);
+        }
+        return sum;
+    }
+
 
 
 
